c5a8975815c2f010b2e3f5b113d4296f901b3bd6
Facundo <acosta.facundo.h@gmail.com>
2020-09-25 00:03:24 -0300

    Calendario

--- proyecto/principal/forms.py
+++ proyecto/principal/forms.py
@@ -2,15 +2,17 @@ from django import forms
 from .models import Publicacion
 import django_filters
 
-
+class DateInput(forms.DateInput):
+	input_type = 'date'
 
 class PublicacionForm(forms.ModelForm):
 	class Meta:
 		model = Publicacion
 		fields = '__all__'
-
+		widgets = {'fecha_fin' : DateInput()}
 
 class PublicacionFilter(django_filters.FilterSet):
 	class Meta:
 		model = Publicacion
 		fields = ['ciudad','tipo','fecha_fin']
+		
